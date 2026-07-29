9dc9d125d0bf84228cff0e366494dd4488fb6932
facusanc <facuu4244@gmail.com>
2026-07-29 20:59:45 -0300

    Primer commit

---
+++ ms-usuarios/src/test/java/ar/edu/utn/frsf/cineapp/CineappApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frsf.cineapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
